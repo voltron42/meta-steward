1534c2c45bd74e01c551dc28649da161e3547989
Daniel Johnson <voltron42@users.noreply.github.com>
2025-04-30 20:26:00 +0000

    adding method for remove title

--- src/meta_steward/core.clj
+++ src/meta_steward/core.clj
@@ -1,7 +1,25 @@
 (ns meta-steward.core
-  (:gen-class))
+  (:gen-class) 
+  (:import
+   [java.io RandomAccessFile]
+   [com.coremedia.iso IsoFile]
+   [com.coremedia.iso.boxes UserDataBox MetaBox]
+   [com.coremedia.iso.boxes.apple AppleItemListBox AppleStringBox]))
+
+(defn remove-title [filePath]
+  (let [raf (RandomAccessFile. filePath "rw")
+        isoFile (IsoFile. (.getChannel raf))
+        moov (.getMovieBox isoFile)
+        udta (-> moov (.getBoxes (.class UserDataBox)) (.get 0))
+        meta (-> udta (.getBoxes (.class MetaBox)) (.get 0))
+        ilst (-> meta (.getBoxes (.class AppleItemListBox)) (.get 0))
+        titleBoxes (-> ilst (.getBoxes (.class AppleStringBox) "@nam"))]
+    (when (not (.isEmpty titleBoxes))
+      (.removeBox ilst (.get titleBoxes 0))
+      (-> raf (.getChannel) (.position 0))
+      (-> isoFile (.writeContainer (-> raf .getChannel))))
+    (.close isoFile)))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [& _]
+  (remove-title "path/to/your/video.mp4"))
